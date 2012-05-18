6e4205ad946783b01f8c4a0c12335e823ff98dda
Alex Porto <alexporto90@gmail.com>
2012-05-17 21:26:39 -0300

    mais uma remocao

--- view/admin/config/iniciarDados.php
+++ view/admin/config/iniciarDados.php
@@ -6,17 +6,4 @@ $nome_site_Title = "CMD - Hoteis & Resorts";
 $cor_principal = "bgAzulClaro";
 $cor_secundaria = "bgAzul";
 
-//echo $_SERVER['PHP_SELF'];
-
-//include('../../../dao/Banco.php');
-//include('../../../dao/Entidade.php');
-//include('../../../dao/UsuarioDao.php');
-//
-//include('../../../bpm/BpmGenerico.php');
-//include('../../../bpm/UsuarioBpm.php');
-//
-//include('../../../vo/UsuarioVo.php');
-//
-//include('../../../biblioteca/funcoes.php');
-
 ?>
