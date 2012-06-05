7b155f0f529fd2c6c501a82d804d3f3d2a395a80
Alex Porto <alexporto90@gmail.com>
2012-06-05 18:41:18 -0300

    Cadastro Hotel Exibição cadastro apartir da Sessão

--- view/admin/hotel/cadastrarHotel.php
+++ view/admin/hotel/cadastrarHotel.php
@@ -1,11 +1,10 @@
 <?php
 include('../template/iniciarDados.php');
-    if ($_SESSION['NIVEL'] === 2) {
-        header('location:cadastrarHotel.php');
-    } else {
-        header('location:../home/');
-    }
-
+if ($_SESSION['NIVEL'] == 2) {
+    header('location:cadastrarHotel.php');
+} else {
+    header('location:../home/');
+}
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
     "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
@@ -24,48 +23,48 @@ include('../template/iniciarDados.php');
 
                     <!-- conteudo -->	
                     <form action="hotelController.php" method="post" id="cadastroHotel" onsubmit="return verificarCampos();" >
-                <input type="hidden" name="acao" id="acao" maxlength="50" value="cadastrarHotel" />
+                        <input type="hidden" name="acao" id="acao" maxlength="50" value="cadastrarHotel" />
 
-                <input type="hidden" name="hotelId" id="hotelId" value="" />
-
-
-                <div id="hotel" >
-                    <!-- hotel -->
-                    <label>Nome do hotel:</label><br />
-                    </label><input type="text" name="hotelNome" id="hotelNome" value="" maxlength="50" class="obrigatorio"  /><br />
-                    E-mail:<br />
-                    <input type="text" name="hotelEmail" id="hotelEmail" value="" maxlength="50" class="obrigatorio"  /><br />
-                    Cnpj: 	<br />
-                    <input type="text" name="hotelCnpj" id="hotelCnpj" value="" maxlength="50" class="obrigatorio"  /><br />
-                    Inscrição Estadual:	<br />
-                    <input type="text" name="hotelInscricaoEstadual" id="hotelInscricaoEstadual" value="" maxlength="50" class="obrigatorio"  /><br />
-                    Confirmacao de senha: 	<br />
-                    <input type="text" name="hotelGerente" id="hotelGerente" value="" maxlength="50" class="obrigatorio"  /><br />
-                    Observação:<br />
+                        <input type="hidden" name="hotelId" id="hotelId" value="" />
 
 
-		        
-			<div id='telefone'>
-				<!-- telefone -->
-			</div>
-			<a onclick="mostrarTelefone();">+ telefone</a><br />
-			<input type='hidden' name="qtdeTelefone" id="qtdeTelefone" value="" />
-			          
-
-                    <label for="cepPesquisa">Cep</label>
-                    <input type="text" 		name="cepPesquisa" 		id="cepPesquisa"    class="cep" />
-                    <input type="button" 	value="Pesquisar cep" onclick="cepPesquisar();" /><br />
-
-                    <input type="hidden" 	name="cepXedicaoTipo" 	id="cepXedicaoTipo" 	value="" />		      
-                    <input type="hidden" 	name="cepXedicaoId" 	id="cepXedicaoId" 	value="" />		  
-                    <div id='cep'>
-                        <!-- cep -->   
-                    </div>
+                        <div id="hotel" >
+                            <!-- hotel -->
+                            <label>Nome do hotel:</label><br />
+                            </label><input type="text" name="hotelNome" id="hotelNome" value="" maxlength="50" class="obrigatorio"  /><br />
+                            E-mail:<br />
+                            <input type="text" name="hotelEmail" id="hotelEmail" value="" maxlength="50" class="obrigatorio"  /><br />
+                            Cnpj: 	<br />
+                            <input type="text" name="hotelCnpj" id="hotelCnpj" value="" maxlength="50" class="obrigatorio"  /><br />
+                            Inscrição Estadual:	<br />
+                            <input type="text" name="hotelInscricaoEstadual" id="hotelInscricaoEstadual" value="" maxlength="50" class="obrigatorio"  /><br />
+                            Confirmacao de senha: 	<br />
+                            <input type="text" name="hotelGerente" id="hotelGerente" value="" maxlength="50" class="obrigatorio"  /><br />
+                            Observação:<br />
 
 
-                    <input type="submit" name="cmdSalvar" value=" Cadastrar " />
-            </form>
-                    
+
+                            <div id='telefone'>
+                                <!-- telefone -->
+                            </div>
+                            <a onclick="mostrarTelefone();">+ telefone</a><br />
+                            <input type='hidden' name="qtdeTelefone" id="qtdeTelefone" value="" />
+
+
+                            <label for="cepPesquisa">Cep</label>
+                            <input type="text" 		name="cepPesquisa" 		id="cepPesquisa"    class="cep" />
+                            <input type="button" 	value="Pesquisar cep" onclick="cepPesquisar();" /><br />
+
+                            <input type="hidden" 	name="cepXedicaoTipo" 	id="cepXedicaoTipo" 	value="" />		      
+                            <input type="hidden" 	name="cepXedicaoId" 	id="cepXedicaoId" 	value="" />		  
+                            <div id='cep'>
+                                <!-- cep -->   
+                            </div>
+
+
+                            <input type="submit" name="cmdSalvar" value=" Cadastrar " />
+                    </form>
+
 
                 </div>
             </div>
